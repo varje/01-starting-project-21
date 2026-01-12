99816dbfdd938a20a02e206e59c80e439f815e9c
varje <varjekivila@gmail.com>
2026-01-12 10:57:10 +0200

    add userslist component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import AddUser from './components/AddUser/AddUser';
+import UsersList from './components/UsersList/UsersList';
 
 function App() {
   return (
     <div>
       <AddUser />
+      <UsersList />
     </div>
   );
 }
---
+++ src/components/UsersList/UsersList.js
@@ -0,0 +1,15 @@
+import classes from './UsersList.module.css';
+
+function UsersList() {
+    return (
+        <div className={classes.usersList}>
+            <ul>
+                <li>
+                    <p>Varje (33 years old)</p>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default UsersList;
