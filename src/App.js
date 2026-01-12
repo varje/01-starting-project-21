028253e625a8d1f9d58f313b442c43f7910618a5
varje <varjekivila@gmail.com>
2026-01-12 11:48:26 +0200

    add add user form submit logic

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AddUser from './components/AddUser/AddUser';
 import UsersList from './components/UsersList/UsersList';
 import Card from './components/Card/Card';
 
 function App() {
+  const [users, setUsers] = useState([]);
+  function addUserHandler(user) {
+    setUsers((prevUsers) => {
+      return [ ...prevUsers, user ];
+    });
+    console.log(users);
+  }
+
   return (
     <div>
       <Card>
-        <AddUser />
+        <AddUser onAddUser={addUserHandler} />
       </Card>
       <Card>
         <UsersList />
--- src/components/AddUser/AddUser.js
+++ src/components/AddUser/AddUser.js
@@ -1,13 +1,34 @@
-import classes from './AddUser.module.css'
+import { useState } from 'react';
+import classes from './AddUser.module.css';
 
-function AddUser() {
+function AddUser(props) {
+  const [user, setUser] = useState({
+    username: '',
+    age: '',
+  });
+
+  function submitHandler(event) {
+    event.preventDefault();
+    props.onAddUser(user);
+  }
+
+  function changeHandler(event) {
+    const { id, value } = event.target;
+    setUser((prevUser) => {
+      return {
+        ...prevUser,
+        [id]: value,
+      };
+    });
+  }
+
   return (
     <div className={classes.addUser}>
-      <form>
+      <form onSubmit={submitHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" />
+        <input id="username" type="text" onChange={changeHandler} />
         <label htmlFor="age">Age(Years)</label>
-        <input id="age" type="number" />
+        <input id="age" type="number" onChange={changeHandler} />
         <button>Add User</button>
       </form>
     </div>
